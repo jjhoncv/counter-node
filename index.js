fb063625731880d2c18ed1e121e53cfdc77558a1
Jhonnatan <jhonnatan.castro@orbis.com.pe>
2019-02-06 18:26:12 -0500

    feat(node): demo counter

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ const exec = require('child_process').exec
 
 process.argv.forEach(function (date, index, array) {
     // 2019-09-24T20:35
-    exec('wget -O ./image/counter.gif https://date-gif.herokuapp.com/generate\?time\='+date+'\&name\=awesome-gif\&frames\=20C')
+    // console.log(new Date().getTime())
+
+    exec('wget -O ./image/aa.gif https://date-gif.herokuapp.com/generate\?time\='+val+'T20:35\&name\=awesome-gif\&frames\=20C')
 });
